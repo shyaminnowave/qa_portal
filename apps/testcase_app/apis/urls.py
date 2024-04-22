d017059791001e99cb01613dfc5aec3da28fa999
shyam.kumar <shyam.kumar@innowave.tech>
2024-04-22 09:58:40 +0530

    testcase-natco list url changed

--- apps/testcase_app/apis/urls.py
+++ apps/testcase_app/apis/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('create/test-case/', views.TestCaseView.as_view()),
     path('test-case/<int:jira_id>/', views.TestCaseDetailView.as_view()),
     path('test-case/natco/<int:jira_id>/', views.TestCaseNatcoView.as_view(), name='testcase-natco'),
-    path('natco/', views.TestCaseNatcoList.as_view(), name='natco-list'),
-    path('test/natco/<int:pk>/', views.TestCaseNatcoDetail.as_view(), name='natco-details'),
+    path('test-case/natco-list/', views.TestCaseNatcoList.as_view(), name='natco-list'),
+    path('test-case/natco-list/<int:pk>/', views.TestCaseNatcoDetail.as_view(), name='natco-details'),
     path('bulk-upload/', views.GetExcel.as_view())
 ]
