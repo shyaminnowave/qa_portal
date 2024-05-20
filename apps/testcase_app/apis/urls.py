922ed158efd399d4cac2290c3e2934384675587e
shyam.kumar <shyam.kumar@innowave.tech>
2024-05-20 13:45:40 +0530

    utility api url removed

--- apps/testcase_app/apis/urls.py
+++ apps/testcase_app/apis/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('test-case/natco-list/', views.TestCaseNatcoList.as_view(), name='natco-list'),
     path('test-case/natco-list/<int:pk>/', views.TestCaseNatcoDetail.as_view(), name='natco-details'),
     path('bulk-upload/', views.GetTestCase.as_view()),
-    path('perfomance-upload', views.GETPERFOMANCEEXCEL.as_view()),
     path('upload/test-result/', views.GetTestResult.as_view()),
     path('report-filter/', views.TestResultFilterView.as_view()),
     path('report-data/', views.TestCaseReportView.as_view())
