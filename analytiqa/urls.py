4c332d4e95c3acf11a4dec119afaee1676bde851
Innodots pro <shyam.kumar@innowave.tech>
2024-09-29 23:21:14 +0530

    urls pattern updated

--- analytiqa/urls.py
+++ analytiqa/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))
 ]
 
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
