ccf0dd26df7789d31ef928dbc7ad1b480f573865
shyaminnowave <shyam.kumar@innowave.tech>
2024-09-30 08:01:12 +0530

    Signals Model updated

--- apps/testcases/signals.py
+++ apps/testcases/signals.py
@@ -9,7 +9,7 @@ from django.shortcuts import get_object_or_404
 @receiver(post_save, sender=TestCaseModel)
 def save_natco_status(sender, instance, created, **kwargs):
     _data = []
-    natco = NactoManufactureLanguage.objects.all()
+    natco = NactoManufacturesLanguage.objects.all()
     if created == True:
         for data in natco:
             _data.append(NatcoStatus(natco=data.natco, language=data.language_name, device=data.device_name,
